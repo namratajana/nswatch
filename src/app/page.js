4f75420b91abb19e2b256bf8ae1f481ab49cdcbc
Biswajit Das <workbiswajit0981@gmail.com>
2025-10-27 12:37:47 +0530

    Refactor `Home` layout: wrap sections with `CustomSection` for improved structure and readability.

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,37 @@
-import Navbar from "@/app/_components/Navbar";
 import HeroSection from "@/app/_components/HeroSection";
 import About from "@/app/_components/About";
 import ProductsSection from "@/app/_components/ProductsSection";
 import TestimonialPage from "@/app/_components/TestimonialPage";
 import NewsletterSubscription from "@/app/_components/NewsletterSubscription";
 import Footer from "@/app/_components/Footer";
+import CustomSection from "@/app/_components/CustomSection";
+import Featured from "@/app/_components/Featured";
 
 
 export default function Home() {
     return (
         <>
-            <Navbar/>
-            <HeroSection/>
-            <About/>
-            <ProductsSection/>
-            <TestimonialPage/>
-            <NewsletterSubscription/>
-            <Footer/>
+            <CustomSection id="home">
+                <HeroSection/>
+            </CustomSection>
+            <CustomSection id="featured">
+                <Featured/>
+            </CustomSection>
+            <CustomSection id="products">
+                <ProductsSection/>
+            </CustomSection>
+            <CustomSection id="about">
+                <About/>
+            </CustomSection>
+            <CustomSection>
+                <TestimonialPage/>
+            </CustomSection>
+            <CustomSection>
+                <NewsletterSubscription/>
+            </CustomSection>
+            <CustomSection>
+                <Footer/>
+            </CustomSection>
         </>
     );
 }
