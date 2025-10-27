9d21a0cd22b948d9f503d18eabc43c1755103fd8
Biswajit Das <workbiswajit0981@gmail.com>
2025-10-27 12:37:28 +0530

    Add `Navbar` component to `RootLayout` and fix code formatting.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,29 +1,31 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
+import Navbar from "@/app/_components/Navbar";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata = {
-  title: "Weyra",
-  description: "Time with a Cat's Eye",
+    title: "Weyra",
+    description: "Time with a Cat's Eye",
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+export default function RootLayout({children}) {
+    return (
+        <html lang="en">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+        <Navbar/>
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
